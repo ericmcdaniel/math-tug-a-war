db8f3e6034e4bc072a49bdd18eda441147181306
Eric McDaniel <mcdanieles@gmail.com>
2023-10-02 22:12:15 -0500

    Convert tabs to spaces

--- backend-api/src/main/java/com/csc478softwareengineeringcapstone/MathTugAWarApplication.java
+++ backend-api/src/main/java/com/csc478softwareengineeringcapstone/MathTugAWarApplication.java
@@ -13,28 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathTugAWarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MathTugAWarApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MathTugAWarApplication.class, args);
+  }
 
-	@GetMapping("/hello")
-	public Map<String, Object> hello(@RequestParam(value = "name", defaultValue = "CSC 478 Group 9") String name) {
+  @GetMapping("/hello")
+  public Map<String, Object> hello(@RequestParam(value = "name", defaultValue = "CSC 478 Group 9") String name) {
 
-		/*
-		 * We want to return a JSON object and not raw text. Hashmaps are perfect for
-		 * this.
-		 * The response data will look like:
-		 * {
-		 * "response": "Hello, <dynamically injected data>",
-		 * "http_status": 200
-		 * }
-		 */
-		Map<String, Object> map = new HashMap<>();
+    /*
+     * We want to return a JSON object and not raw text. Hashmaps are perfect for
+     * this.
+     * The response data will look like:
+     * {
+     * "response": "Hello, <dynamically injected data>",
+     * "http_status": 200
+     * }
+     */
+    Map<String, Object> map = new HashMap<>();
 
-		map.put("response", String.format("Hello %s!", name));
-		map.put("http_status", 200);
+    map.put("response", String.format("Hello %s!", name));
+    map.put("http_status", 200);
 
-		return map;
-	}
+    return map;
+  }
 
 }
