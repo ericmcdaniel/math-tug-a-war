cc01b2915b55d59f1f531bf4ff1b5b2a3241e5a4
Eric McDaniel <mcdanieles@gmail.com>
2023-10-18 04:44:32 -0500

    Setup backend API CI/CD pipeline

--- frontend-ui/src/app/app.component.spec.ts
+++ frontend-ui/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend-ui'`, () => {
+  it(`should have as title 'Math Tug-a-War'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-ui');
+    expect(app.title).toEqual('Math Tug-a-War');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Math Tug-a-War app is running!');
   });
 });
