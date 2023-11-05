6c7ea83ef33c234d4ed0332684ff4d113cc525b4
Eric McDaniel <mcdanieles@gmail.com>
2023-11-05 01:44:14 -0600

    Add User state to simulate logging in; alleviate cold start time
    #minor bump

---
+++ frontend-ui/src/app/core/services/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private user$: BehaviorSubject<boolean>;
+
+  constructor(private _http: HttpClient) {
+    this.user$ = new BehaviorSubject<boolean>(false);
+  }
+
+  getUser(): Observable<boolean> {
+    return this.user$.asObservable();
+  }
+
+  logInUser(): void {
+    this._http.get<{ status: string; }>(environment.apiUrl + 'health-test').subscribe({
+      next: response => {
+        if (response.status === 'OK')
+          this.user$.next(true);
+      },
+      error: (error) => { throw new Error(error); }
+    });
+  }
+}
--- frontend-ui/src/app/feature/game/components/configure/configure.component.ts
+++ frontend-ui/src/app/feature/game/components/configure/configure.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../../../core/services/user.service';
 
 @Component({
   selector: 'app-configure',
   templateUrl: './configure.component.html',
   styleUrls: ['./configure.component.css']
 })
-export class ConfigureComponent {
+export class ConfigureComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(public user: UserService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.user.getUser().subscribe(user => {
+      if (!user) this.user.logInUser();
+    });
+  }
 
   difficulty = ['Easy', 'Medium', 'Hard'];
   selectedDifficulty = '--';
-
 
   onSubmit(form: NgForm) {
     this.router.navigate(['play'], { relativeTo: this.route });
