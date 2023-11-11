536e6d7e81707e333a8a3176f2c11c593ad704f1
Eric McDaniel <mcdanieles@gmail.com>
2023-11-10 22:59:13 -0600

    semi-working animation

--- frontend-ui/src/app/core/pages/landing/landing.page.ts
+++ frontend-ui/src/app/core/pages/landing/landing.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -6,13 +7,21 @@ import { UserService } from '../../services/user.service';
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.css']
 })
-export class LandingPage implements OnInit {
+export class LandingPage implements AfterViewInit {
+
+  displayOp1 = new BehaviorSubject<number>(Math.floor(Math.random() * 10));
+  displayOp2 = new BehaviorSubject<number>(Math.floor(Math.random() * 10));
 
   constructor(private user: UserService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.user.getUser().subscribe(user => {
       if (!user) this.user.logInUser();
     });
+
+    // timer(8500).subscribe(() => this.displayOp1.next(Math.floor(Math.random() * 10)));
+    // interval(12000).subscribe(() => this.displayOp1.next(Math.floor(Math.random() * 10)));
   }
+
+
 }
