8f79a69a45f91d2cd79661c62b9e507ab4c80a1b
Eric McDaniel <mcdanieles@gmail.com>
2023-11-19 02:56:19 -0600

    Style Page Not Found component

--- frontend-ui/src/app/core/pages/not-found/not-found.page.ts
+++ frontend-ui/src/app/core/pages/not-found/not-found.page.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  template: '<p>404 Error: Page was not found.</p>'
+  template: `
+  <div class="card-container">
+    <div class="card-wrapper">
+      <h1 class="card-display-title">Feeling lost?</h1>
+      <h2 class="card-display-subtitle">The page you are looking for could not be found</h2>
+      <div class="return-link-container">
+        <a class="return-link" routerLink="/">Go back home</a>   
+      </div>
+    </div>
+  </div>
+  `
 })
 export class NotFoundPage { }
